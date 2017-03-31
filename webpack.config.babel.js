be21b2f68033dfd947bfc1ad47a97131d686b8c7
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-03-31 17:18:35 +0900

    init

---
+++ webpack.config.babel.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+module.exports = [
+  {
+    name: 'js',
+    entry: path.join(__dirname, 'src', 'js', 'index.js'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    target: 'web',
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  },
+  {
+    name: 'html',
+    entry: path.join(__dirname, 'src', 'index.html'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'index.html',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract('raw-loader'),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('index.html'),
+    ],
+  },
+  {
+    name: 'style',
+    entry: path.join(__dirname, 'src', 'sass', 'style.sass'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'style.css',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.sass$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract(['css-loader', 'sass-loader', 'import-glob-loader']),
+        },
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          loader: 'url-loader',
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css'),
+    ],
+  },
+];
